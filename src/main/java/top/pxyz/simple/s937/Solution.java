0b3c0825f55cdb8e5d4b0205526af4f85ed01e66
linchao <15732749090@163.com>
2022-05-07 23:07:24 +0800

    feat: s937：重新排列日志文件

---
+++ src/main/java/top/pxyz/simple/s937/Solution.java
@@ -0,0 +1,20 @@
+package top.pxyz.simple.s937;
+
+import java.util.Arrays;
+
+/**
+ * @author Ijiran
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.reorderLogFiles(new String[]{"dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"})));
+    }
+
+    public String[] reorderLogFiles(String[] logs) {
+
+        return null;
+    }
+
+}
---
+++ src/main/java/top/pxyz/simple/s937/readme.md
@@ -0,0 +1,15 @@
+#### 题目
+
+[重新排列日志文件](https://leetcode-cn.com/problems/reorder-data-in-log-files/)
+
+#### 考点
+
+
+
+#### 关键点
+
+
+
+#### 执行结果
+
+
